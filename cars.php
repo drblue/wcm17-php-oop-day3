14a6d7ec7f5dfc1ee7df67e12035b3236b2984b4
Johan Nordström <johan@digitalvillage.se>
2018-02-16 11:53:47 +0100

    Uppgift 5 klar.

--- cars.php
+++ cars.php
@@ -4,13 +4,16 @@ require "includes/Car.php";
 
 $cars = [];
 
-$car = new Car("Nissan", "Qashqai");
+$car = new Car("Nissan", "Qashqai", 2008, "LJU460", 161000);
+$car->drive(570);
+$car->drive(48);
+$car->drive(-48000);
 array_push($cars, $car);
 
-$car = new Car("Porsche", "911", 1997, "SPEEED");
+$car = new Car("Porsche", "911", 1997, "SPEEED", 25000);
 array_push($cars, $car);
 
-$car = new Car("Mini", "Cooper", 2005, "MINI2");
+$car = new Car("Mini", "Cooper");
 array_push($cars, $car);
 
 foreach ($cars as $car) {
--- includes/Car.php
+++ includes/Car.php
@@ -7,16 +7,33 @@ class Car {
 	protected $registrationNumber;
 	protected $mileage;
 
-	public function __construct($manufacturer, $model, $year = 2018, $registrationNumber = "N/A", $mileage = 0) {
+	public function __construct($manufacturer, $model, $year = null, $registrationNumber = "N/A", $mileage = 0) {
 		$this->manufacturer = $manufacturer;
 		$this->model = $model;
-		$this->year = $year;
+
+		if ($year === null) {
+			// hämta nuvarande år
+			$presentYear = date("Y");
+			$this->year = $presentYear;
+		} else {
+			$this->year = $year;
+		}
+		
 		$this->registrationNumber = $registrationNumber;
 		$this->mileage = $mileage;
 	}
 
 	public function getMileage() {
-		return $this->mileage;
+		$milageInMil = $this->mileage / 10;
+		return number_format($milageInMil, 1);
+
+		// samma sak som ovan, bara på en rad
+		return number_format($this->milage / 10, 1);
+	}
+	public function drive($kilometers) {
+		if ($kilometers > 0) {
+			$this->mileage = $this->mileage + $kilometers;
+		}
 	}
 
 	public function getManufacturer() {
@@ -50,10 +67,10 @@ class Car {
 	public function getInfo() {
 		// detta är tre olika sätt att göra samma sak på
 
-		return "Jag är en " . $this->manufacturer . " " . $this->model . " av årsmodell " . $this->year . " med registreringsnummer " . $this->registrationNumber . ".";
+		// return "Jag är en " . $this->manufacturer . " " . $this->model . " av årsmodell " . $this->year . " med registreringsnummer " . $this->registrationNumber . ".";
 
-		return "Jag är en {$this->manufacturer} {$this->model} av årsmodell {$this->year} med registreringsnummer {$this->registrationNumber}.";
+		return "Jag är en {$this->manufacturer} {$this->model} av årsmodell {$this->year} med registreringsnummer {$this->registrationNumber}. Min mätarställning är {$this->getMileage()} mil.";
 
-		return sprintf("Jag är en %s %s av årsmodell %s med registreringsnummer %s.", $this->manufacturer, $this->model, $this->year, $this->registrationNumber);
+		// return sprintf("Jag är en %s %s av årsmodell %s med registreringsnummer %s.", $this->manufacturer, $this->model, $this->year, $this->registrationNumber);
 	}
 }
